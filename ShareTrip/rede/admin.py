123a2b1b78320af94e926bd05b29d300d06e779f
sebastian <sebasebastiaolacerda@outlook.com>
2017-09-18 12:20:23 -0300

    progresso

--- ShareTrip/rede/admin.py
+++ ShareTrip/rede/admin.py
@@ -3,4 +3,48 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from rede import models
+from forms import UsuarioForm
+
 # Register your models here.
+
+#===============================================================================
+# Usuario
+#===============================================================================
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf')
+    form = UsuarioForm
+
+admin.site.register(models.Usuario, UsuarioAdmin)
+
+#===============================================================================
+# Onibus
+#===============================================================================
+class OnibusAdmin(admin.ModelAdmin):
+    list_display = ('motorista', 'modelo')
+
+admin.site.register(models.Onibus, OnibusAdmin)
+
+#===============================================================================
+# Empresa
+#===============================================================================
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+admin.site.register(models.Empresa, EmpresaAdmin)
+
+#===============================================================================
+# Viagem
+#===============================================================================
+class ViagemAdmin(admin.ModelAdmin):
+    list_display = ('origem', 'destino', 'data')
+
+admin.site.register(models.Viagem, ViagemAdmin)
+
+#===============================================================================
+# Bilhete
+#===============================================================================
+class BilheteAdmin(admin.ModelAdmin):
+    list_display = ('passageiro', 'Id')
+
+admin.site.register(models.Bilhete, BilheteAdmin)
--- ShareTrip/rede/models.py
+++ ShareTrip/rede/models.py
@@ -1,9 +1,128 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
 
-class User(models.Model):
-    pass
+    nome = models.CharField(
+    'nome',
+    max_length = 255
+    )
+    telefone = models.CharField(
+    'telefone',
+    max_length=255,
+    )
+    cpf = models.CharField(
+    'cpf',
+    max_length = 14,
+    )
+
+    senha = models.CharField(
+    'Senha',
+    max_length = 255
+    )
+
+    class Meta:
+        verbose_name = 'usuario'
+        verbose_name_plural = 'usuarios'
+        ordering = ["nome"]
+
+
+class Onibus(models.Model):
+
+    motorista = models.CharField(
+    'Motorista',
+    max_length = 255
+    )
+    modelo = models.CharField(
+    'Modelo',
+    max_length=255
+    )
+
+
+    class Meta:
+        verbose_name = 'onibus'
+        verbose_name_plural = 'onibus'
+        ordering = ["motorista"]
+
+
+class Empresa(models.Model):
+
+    nome = models.CharField(
+    'nome',
+    max_length = 255
+    )
+    endereco = models.CharField(
+    'Endereço',
+    max_length = 255
+    )
+
+    class Meta:
+        verbose_name = 'empresa'
+        verbose_name_plural = 'empresas'
+        ordering = ["nome"]
+
+class Viagem(models.Model):
+
+    origem = models.CharField(
+    'origem',
+    max_length = 255
+    )
+    destino = models.CharField(
+    'destino',
+    max_length = 255
+    )
+    horaS = models.CharField(
+    'Hora de Saida',
+    max_length = 255
+    )
+    horaC = models.CharField(
+    'Hora de Chegada',
+    max_length = 255
+    )
+    empresa = models.ForeignKey(
+    'Empresa',
+    on_delete=models.CASCADE,
+    related_name = "empresa"
+    )
+    onibus = models.ForeignKey(
+    'Onibus',
+    on_delete=models.CASCADE,
+    related_name = "Onibus"
+    )
+    data = models.DateTimeField(
+        'Data',
+        default=datetime.now()
+    )
+
+    class Meta:
+        verbose_name = 'viagem'
+        verbose_name_plural = 'viagens'
+        ordering = ["data"]
+
+
+class Bilhete (models.Model):
+
+    poltrona = models.IntegerField(
+    'poltrona',
+    )
+
+    passageiro = models.ForeignKey(
+    "Usuario",
+    on_delete=models.CASCADE,
+    related_name = "passageiro"
+    )
+
+    Id = models.ForeignKey(
+    "Viagem",
+    on_delete = models.CASCADE,
+    related_name = "viagem"
+    )
+    class Meta:
+        verbose_name = 'bilhete'
+        verbose_name_plural = 'bilhetes'
+        ordering = ["passageiro"]
