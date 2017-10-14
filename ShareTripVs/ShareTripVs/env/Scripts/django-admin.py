f1f63d41082548060a4242c2b3dcb8d6d47c2abb
sebastiaoLa <sebastiaolacerda@outlook.com>
2017-10-14 20:36:12 -0300

    removed env and a few useless files

--- ShareTripVs/ShareTripVs/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Documentos\GitHub\ShareTrip\ShareTripVs\ShareTripVs\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
